1332a90615ebd745af8e395ae2226b2b9a10f0f6
Zacharie Montreuil <zmontreuil@rrc.ca>
2025-04-09 14:44:55 -0500

    Added unit tests for js

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// business logic function
+// "pure" function: no side-effects, it receives all data from arguments, 
+// and only returns a value
 function addNumbers(num1, num2) {
     return num1 + num2;
 }
@@ -5,12 +8,15 @@ function addNumbers(num1, num2) {
 function updateDom(value) {
     // remember: Node does not have a document to work with
     // the document is provided by the browser Document Object Model
-    let resultDivNode = document.querySelector("#result");
-    if(resultDivNode) {
-        resultDivNode.innerText = `Result of the operation is ${value}`;
+    // function also takes a value from outside its arguments (the html element)
+    let resultNode = document.querySelector("#result");
+    if(resultNode) {
+        // "side-effect": modifies something outside of the return
+        resultNode.textContent = `Result of the operation is ${value}`;
     }
 }
 
+// "integration": get a value from addNumbers and use it to update the dom
 function init() {
     const result = addNumbers(10, 20);
     updateDom(result);
--- script.test.js
+++ script.test.js
@@ -1,2 +1,32 @@
 // import functions from another file for use in this one
 const {addNumbers, updateDom} = require("./script");
+
+// import JSDOM object from package
+const { JSDOM } = require("jsdom");
+
+// "function-context-expected outcome"
+test("addNumbers with correct arguments returns a sum", () => {
+    // arrange
+    const operandOne = 10;
+    const operandTwo = 20;
+
+    // act
+    const returnedValue = addNumbers(operandOne, operandTwo);
+
+    // assert
+    expect(returnedValue).toBe(30);
+});
+
+test("updateDom with valid args adds updates text in html element", () => {
+    // create new "fake DOM" containing our test HTML
+    const testDom = new JSDOM(`<!DOCTYPE html><p id="result">Hello world</p>`);
+
+    // set the fake dom as the global.dom so that our function uses it as the document
+    global.document = testDom.window.document;
+    const testValue = 30;
+
+    updateDom(testValue);
+
+    const actualHTML = global.document.querySelector("#result").textContent;
+    expect(actualHTML).toBe("Result of the operation is 30");
+});
